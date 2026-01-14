a1e10ef007fe186e220049eed998bf6f2fe2422e
Willow Ahrens <willow@csail.mit.edu>
2026-01-14 12:15:18 -0500

    okay, basically there

--- conv.cpp
+++ conv.cpp
@@ -6,7 +6,7 @@
 namespace fs = std::filesystem;
 
 template <typename T, typename I>
-void experiment_spmv_csr(benchmark_params_t params);
+void experiment_conv(benchmark_params_t params);
 
 int main(int argc, char **argv){
     auto params = parse(argc, argv);
@@ -31,52 +31,39 @@ int main(int argc, char **argv){
     return 0;
 }
 
-template <typename T, typename I>
-void experiment_spmv_csr(benchmark_params_t params){
-    auto A_desc = json::parse(std::ifstream(fs::path(params.input)/"A.bspnpy"/"binsparse.json"))["binsparse"]; 
-    auto x_desc = json::parse(std::ifstream(fs::path(params.input)/"x.bspnpy"/"binsparse.json"))["binsparse"]; 
-
+void experiment_conv(benchmark_params_t params){
     int m = A_desc["shape"][0];
     int n = A_desc["shape"][1];
 
-    auto x_val = npy_load_vector<T>(fs::path(params.input)/"x.bspnpy"/"values.npy");
-    auto A_ptr = npy_load_vector<I>(fs::path(params.input)/"A.bspnpy"/"pointers_to_1.npy");
-    auto A_idx = npy_load_vector<I>(fs::path(params.input)/"A.bspnpy"/"indices_1.npy");
-    auto A_val = npy_load_vector<T>(fs::path(params.input)/"A.bspnpy"/"values.npy");
+    auto A = npy_load_vector<double>(fs::path(params.input)/"A.npy");
 
-    auto y_val = std::vector<T>(m, 0);
+    B = malloc(m * n * sizeof(double));
 
     //perform an spmv of the matrix in c++
 
     auto time = benchmark(
     []() {
     },
-        [&y_val, &A_ptr, &A_val, &A_idx, &x_val, &m, &n]() {
-            for(int i = 0; i < m; i++){
-                for(int p = A_ptr[i]; p < A_ptr[i+1]; p++){
-                    int j = A_idx[p];
-                    y_val[i] += A_val[p] * x_val[j];
+        [&A_val, &m, &n]() {
+            for(int x = 0; x < m; x++){
+                for (int y = 0; y < n; y++){
+                    for (int dx = -1; dx <= 1; dx++){
+                        for (int dy = -1; dy <= 1; dy++){
+                            if(x + dx >= 0 && x + dx < m && y + dy >= 0 && y + dy < n){
+                                B[x*n + y] += A[(x+dx)*n + (y+dy)];
+                            }
+                        }
+                    }
+                    
                 }
             }
         }
     );
 
-    std::filesystem::create_directory(fs::path(params.output)/"y.bspnpy");
-    json y_desc;
-    y_desc["version"] = 0.5;
-    y_desc["format"] = "DVEC";
-    y_desc["shape"] = {n};
-    y_desc["nnz"] = n;
-    y_desc["data_types"]["values_type"] = "float64";
-    std::ofstream y_desc_file(fs::path(params.output)/"y.bspnpy"/"binsparse.json");
-    y_desc_file << y_desc;
-    y_desc_file.close();
-
-    npy_store_vector<T>(fs::path(params.output)/"y.bspnpy"/"values.npy", y_val);
+    npy_store_vector<T>(fs::path(params.output)/"B.bspnpy", y_val);
 
     json measurements;
     measurements["time"] = time;
-    measurements["memory"] = 0;
     std::ofstream measurements_file(fs::path(params.output)/"measurements.json");
     measurements_file << measurements;
     measurements_file.close();
